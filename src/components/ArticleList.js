9565121111dda9cfbcbec6c2f4ebb1af264efda7
Tammy <tammycwong@gmail.com>
2021-04-06 22:11:34 -0400

    WINNER

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Article from "./Article.js"
 
 function ArticleList({posts}) {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 function Header ({name}) {
     return (
     <header>
